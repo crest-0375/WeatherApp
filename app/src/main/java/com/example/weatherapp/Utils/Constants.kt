10c64ba28685a0b5bf687c797b6906d3fa4a69c0
Jemish Khunt <jemish.k@crestinfosystems.com>
2024-05-30 13:08:30 +0530

    Revert "Updated Network access state"
    This reverts commit 245d84d284c993e54d75145dad9097c31bc3dbb1.

--- app/src/main/java/com/example/weatherapp/Utils/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Utils/Constants.kt
@@ -17,13 +17,18 @@ object Constants {
 
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network      = connectivityManager.activeNetwork ?: return false
-        val activeNetWork = connectivityManager.getNetworkCapabilities(network) ?: return false
-        return when {
-            activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            else -> false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network      = connectivityManager.activeNetwork ?: return false
+            val activeNetWork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            val networkInfo = connectivityManager.activeNetworkInfo
+            return networkInfo != null && networkInfo.isConnectedOrConnecting
         }
     }
 }
